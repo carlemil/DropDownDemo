60c7ad4687d53f1a85e921db41209812705af4b4
Carl-Emil Kjellstrand <erbsman@gmail.com>
2012-11-11 14:06:40 +0000

    first working version of demo

--- src/se/kjellstrand/dropdowndemo/MainActivity.java
+++ src/se/kjellstrand/dropdowndemo/MainActivity.java
@@ -1,103 +1,131 @@
+
 package se.kjellstrand.dropdowndemo;
 
-import android.app.ActionBar;
-import android.app.FragmentTransaction;
-import android.content.Context;
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.NavUtils;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.Animation.AnimationListener;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends FragmentActivity implements ActionBar.OnNavigationListener {
+public class MainActivity extends Activity {
 
-    private static final String STATE_SELECTED_NAVIGATION_ITEM = "selected_navigation_item";
+    /** Layout holding the droddown view */
+    private LinearLayout mDropdownFoldOutMenu;
+
+    /** Textview holding the title of the droddown */
+    private TextView mDropdownTitle;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
 
-        // Set up the action bar.
-        final ActionBar actionBar = getActionBar();
-        actionBar.setDisplayShowTitleEnabled(false);
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+        mDropdownFoldOutMenu = ((LinearLayout) findViewById(R.id.dropdown_foldout_menu));
+        mDropdownTitle = ((TextView) findViewById(R.id.dropdown_textview));
 
-        // Set up the dropdown list navigation in the action bar.
-        actionBar.setListNavigationCallbacks(
-                // Specify a SpinnerAdapter to populate the dropdown list.
-                new ArrayAdapter(
-                        actionBar.getThemedContext(),
-                        android.R.layout.simple_list_item_1,
-                        android.R.id.text1,
-                        new String[]{
-                                getString(R.string.title_section1),
-                                getString(R.string.title_section2),
-                                getString(R.string.title_section3),
-                        }),
-                this);
-    }
+        final TextView dropDownTextView = (TextView) findViewById(R.id.dropdown_textview);
+        final TextView alt0 = (TextView) findViewById(R.id.dropdown_alt0);
+        final TextView alt1 = (TextView) findViewById(R.id.dropdown_alt1);
+        final TextView alt2 = (TextView) findViewById(R.id.dropdown_alt2);
 
-    @Override
-    public void onRestoreInstanceState(Bundle savedInstanceState) {
-        if (savedInstanceState.containsKey(STATE_SELECTED_NAVIGATION_ITEM)) {
-            getActionBar().setSelectedNavigationItem(
-                    savedInstanceState.getInt(STATE_SELECTED_NAVIGATION_ITEM));
-        }
-    }
+        dropDownTextView.setOnClickListener(
+                new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (mDropdownFoldOutMenu.getVisibility() == View.GONE) {
+                            openDropdown();
+                        } else {
+                            closeDropdown();
+                        }
+                    }
+                });
 
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        outState.putInt(STATE_SELECTED_NAVIGATION_ITEM,
-                getActionBar().getSelectedNavigationIndex());
-    }
+        alt0.setOnClickListener(
+                new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dropDownTextView.setText(R.string.alt0);
+                        closeDropdown();
+                        alt0.setCompoundDrawablesWithIntrinsicBounds(0, 0,
+                                R.drawable.icn_dropdown_checked, 0);
+                        alt1.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
+                        alt2.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
+                        Toast.makeText(getBaseContext(), R.string.alt0, Toast.LENGTH_SHORT).show();
+                    }
+                });
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+        alt1.setOnClickListener(
+                new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dropDownTextView.setText(R.string.alt1);
+                        closeDropdown();
+                        alt0.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
+                        alt1.setCompoundDrawablesWithIntrinsicBounds(0, 0,
+                                R.drawable.icn_dropdown_checked, 0);
+                        alt2.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
+                        Toast.makeText(getBaseContext(), R.string.alt1, Toast.LENGTH_SHORT).show();
+                    }
+                });
 
-    
-
-    @Override
-    public boolean onNavigationItemSelected(int position, long id) {
-        // When the given tab is selected, show the tab contents in the container
-        Fragment fragment = new DummySectionFragment();
-        Bundle args = new Bundle();
-        args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position + 1);
-        fragment.setArguments(args);
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.container, fragment)
-                .commit();
-        return true;
+        alt2.setOnClickListener(
+                new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dropDownTextView.setText(R.string.alt2);
+                        closeDropdown();
+                        alt0.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
+                        alt1.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
+                        alt2.setCompoundDrawablesWithIntrinsicBounds(0, 0,
+                                R.drawable.icn_dropdown_checked, 0);
+                        Toast.makeText(getBaseContext(), R.string.alt2, Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     /**
-     * A dummy fragment representing a section of the app, but that simply displays dummy text.
+     * Animates in the dropdown list
      */
-    public static class DummySectionFragment extends Fragment {
-        public DummySectionFragment() {
+    private void openDropdown() {
+        if (mDropdownFoldOutMenu.getVisibility() != View.VISIBLE) {
+            ScaleAnimation anim = new ScaleAnimation(1, 1, 0, 1);
+            anim.setDuration(getResources().getInteger(R.integer.dropdown_amination_time));
+            mDropdownFoldOutMenu.startAnimation(anim);
+            mDropdownTitle.setCompoundDrawablesWithIntrinsicBounds(0, 0,
+                    R.drawable.icn_dropdown_close, 0);
+            mDropdownFoldOutMenu.setVisibility(View.VISIBLE);
         }
+    }
 
-        public static final String ARG_SECTION_NUMBER = "section_number";
+    /**
+     * Animates out the dropdown list
+     */
+    private void closeDropdown() {
+        if (mDropdownFoldOutMenu.getVisibility() == View.VISIBLE) {
+            ScaleAnimation anim = new ScaleAnimation(1, 1, 1, 0);
+            anim.setDuration(getResources().getInteger(R.integer.dropdown_amination_time));
+            anim.setAnimationListener(new AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+                }
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            TextView textView = new TextView(getActivity());
-            textView.setGravity(Gravity.CENTER);
-            Bundle args = getArguments();
-            textView.setText(Integer.toString(args.getInt(ARG_SECTION_NUMBER)));
-            return textView;
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    mDropdownFoldOutMenu.setVisibility(View.GONE);
+                }
+            });
+            mDropdownTitle.setCompoundDrawablesWithIntrinsicBounds(0, 0,
+                    R.drawable.icn_dropdown_open, 0);
+            mDropdownFoldOutMenu.startAnimation(anim);
         }
     }
 }
